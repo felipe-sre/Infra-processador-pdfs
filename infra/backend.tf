e660002bb260c4ae60beded9bab4c1e44281bbfe
Felipe Santana <devops.azure.lfsg@gmail.com>
2026-08-09 00:52:22 -0300

    fix: adiciona configuracao do S3 no backend e desabilita input no terraform init

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,5 +1,7 @@
 terraform {
   backend "s3" {
-    # Backend S3 configurado via pipeline ou variáveis
+    bucket = "proc-pdfs-bucket-tfstate" # Informe o nome do seu bucket de estado S3
+    key    = "pipeline-pdfs/dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
